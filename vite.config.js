b6cb821dcf582872fdee71ddbf0359bc9acfcf66
Elie <rodrigueapothey@gmail.com>
2026-06-10 14:42:37 +0100

    commit dockerfile viteconfigjs 1006

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,29 @@
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//            input: [
+//                 'resources/sass/app.scss',
+//                 'resources/js/app.js',
+//                 'resources/assets/css/theme.min.css',
+//             ],
+//             refresh: true,
+//         }),
+//     ],
+
+// });
+
+// nouveau vite js
+
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-           input: [
+            input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
                 'resources/assets/css/theme.min.css',
@@ -12,5 +31,15 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-
+    build: {
+        manifest: true,
+        rollupOptions: {
+            output: {
+                assetFileNames: 'assets/[name]-[hash][extname]',
+                chunkFileNames: 'assets/[name]-[hash].js',
+                entryFileNames: 'assets/[name]-[hash].js',
+            }
+        }
+    },
+    base: '/',  // ← Force les chemins relatifs pour la production
 });
