2ac648e4089afbc1d295d3a43568cbfddd6aa8a8
Elie <rodrigueapothey@gmail.com>
2026-08-21 20:54:30 +0100

    commit dockerfile et entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,17 @@
 mkdir -p /var/www/html/storage/logs
 mkdir -p /var/www/html/bootstrap/cache
 
-# Forcer les permissions avec root
+# Forcer les permissions
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
 
-# Fichier SQLite
-touch /var/www/html/database/database.sqlite
-chown www-data:www-data /var/www/html/database/database.sqlite
-chmod 664 /var/www/html/database/database.sqlite
+# Fichier SQLite dans /tmp (accessible en écriture)
+touch /tmp/database.sqlite
+chown www-data:www-data /tmp/database.sqlite
+chmod 664 /tmp/database.sqlite
+
+# Lien symbolique vers /tmp pour que Laravel le trouve
+ln -sf /tmp/database.sqlite /var/www/html/database/database.sqlite
 
 # Exécuter les migrations
 php artisan migrate --seed --force
