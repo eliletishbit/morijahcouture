e89483cf4ca23034224344e8b8db5a1671f8be61
Elie <rodrigueapothey@gmail.com>
2026-08-21 20:26:20 +0100

    ajout entrypoint version root

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# Créer les dossiers
+# Créer les dossiers s'ils n'existent pas
 mkdir -p /var/www/html/storage/logs
 mkdir -p /var/www/html/bootstrap/cache
 
-# Forcer les permissions
-chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
-chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+# Forcer les permissions avec sudo
+sudo chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+sudo chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
 
-# Fichier SQLite
+# Créer le fichier SQLite
 touch /var/www/html/database/database.sqlite
-chown www-data:www-data /var/www/html/database/database.sqlite
-chmod 664 /var/www/html/database/database.sqlite
+sudo chown www-data:www-data /var/www/html/database/database.sqlite
+sudo chmod 664 /var/www/html/database/database.sqlite
 
-# Migrations
+# Lancer les migrations
 php artisan migrate --seed --force
 
-# Services
-service nginx start
-php-fpm -F
+# Démarrer les services
+sudo service nginx start
+sudo php-fpm -F
